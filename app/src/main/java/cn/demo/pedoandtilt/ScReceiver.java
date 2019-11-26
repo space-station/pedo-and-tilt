87292910a4d962ed202148aa3f99f2e663e38404
b103 <yu.tian@borqs.net>
2019-11-26 21:01:45 +0800

    upload the app skeleton

---
+++ app/src/main/java/cn/demo/pedoandtilt/ScReceiver.java
@@ -0,0 +1,34 @@
+package cn.demo.pedoandtilt;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+public class ScReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "ScReceiver";
+
+    public ScReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String host = null;
+        Uri uri = intent.getData();
+        if (uri != null) {
+            host = uri.getHost();
+        } else {
+            Log.d(TAG, "uri is null");
+            return;
+        }
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if ("6868".equals(host) || "666".equals(host)) {
+            i.setClass(context, MainActivity.class);
+            context.startActivity(i);
+        }
+    }
+}
